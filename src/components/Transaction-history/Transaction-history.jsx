20365d7b0bca9e1cb4c8598d4b02ad61bd3b6d1a
PavlenkoVolodymyr <volodymyrp88@gmail.com>
2021-11-28 12:18:39 +0200

    1

--- src/components/Transaction-history/Transaction-history.jsx
+++ src/components/Transaction-history/Transaction-history.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import s from './Transaction-history.modules.sass';
 
-function TransactionHistory({ id, type, amount, currency }) {
+function TransactionHistory({ transactions }) {
   return (
-    <table class="transaction-history">
+    <table>
       <thead>
         <tr>
           <th>Type</th>
@@ -13,18 +13,27 @@ function TransactionHistory({ id, type, amount, currency }) {
       </thead>
 
       <tbody>
-        <tr>
-          <td>Invoice</td>
-          <td>125</td>
-          <td>USD</td>
-        </tr>
-        <tr>
-          <td>Withdrawal</td>
-          <td>85</td>
-          <td>USD</td>
-        </tr>
+        {transactions.map(el => (
+          <tr key={el.id} className={s.row}>
+            <td>{el.type}</td>
+            <td>{el.amount}</td>
+            <td>{el.currency}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
 }
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
 export default TransactionHistory;
