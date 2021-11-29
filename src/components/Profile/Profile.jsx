cf36579776fe278e0443fec007178d9cece014d3
PavlenkoVolodymyr <volodymyrp88@gmail.com>
2021-11-29 12:13:25 +0200

    scss profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,7 +8,7 @@ function Profile({ username, tag, location, avatar, stats = {} }) {
       <div className={s.description}>
         <img src={avatar} alt="User avatar" className={s.avatar} />
         <p className={s.name}>{username}</p>
-        <p className={s.tag}>{tag}</p>
+        <p className={s.tag}>@{tag}</p>
         <p className={s.location}>{location}</p>
       </div>
 
